c3ea8aac9a66fc1688e64280db8780e6734261b1
marwa mazini <marwa6mzi@gmail.com>
2023-11-20 11:59:13 +0100

    message

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,39 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Navbar as BootstrapNavbar, Nav } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './NavBar.css';
-
 
 export default function Navbar({ toggleDarkMode, isDarkMode }) {
     return (
-        <nav className={`navbar navbar-expand-lg nav-top ${isDarkMode ? 'dark-mode' : 'light-mode'}`}>
+        <BootstrapNavbar className={`nav-top ${isDarkMode ? 'dark-mode' : 'light-mode'}`} expand="lg">
             <Link className="navbar-brand p-3" to="/">Home</Link>
-            <button className="navbar-toggler btn btn-info text-white mx-2" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/calculator">CALCULATOR</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/slider">SLIDER</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/TODO">TO DO LIST</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/Pag">PAGINATION</Link>
-                    </li>
-                    <li className="nav-item">
-                        <Link className="nav-link" to="/lang">MULTILANGUAGE</Link>
-                    </li>
-                    <div className="toggle-mode-btn">
-                        <button onClick={toggleDarkMode}>
-                            {isDarkMode ? 'Light Mode' : 'Dark Mode'}
-                        </button>
-                    </div>
-                </ul>
-            </div>
-        </nav>
+            <BootstrapNavbar.Toggle aria-controls="basic-navbar-nav" />
+            <BootstrapNavbar.Collapse id="basic-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link as={Link} to="/calculator">CALCULATOR</Nav.Link>
+                    <Nav.Link as={Link} to="/slider">SLIDER</Nav.Link>
+                    <Nav.Link as={Link} to="/TODO">TO DO LIST</Nav.Link>
+                    <Nav.Link as={Link} to="/Pag">PAGINATION</Nav.Link>
+                    <Nav.Link as={Link} to="/lang">MULTILANGUAGE</Nav.Link>
+                </Nav>
+                <div className="toggle-mode-btn">
+                    <button onClick={toggleDarkMode}>
+                        {isDarkMode ? 'Light Mode' : 'Dark Mode'}
+                    </button>
+                </div>
+            </BootstrapNavbar.Collapse>
+        </BootstrapNavbar>
     );
 }
